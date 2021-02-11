9fe04ecf4e2762cc1e59cf20bdf40b5e4f59b648
unknown <weeslleey354@gmail.com>
2021-02-10 22:28:13 -0300

    create login

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
+import UserController from "./app/controller/UserController";
 
 const app = express();
 
-app.post("");
+app.use(express.json());
+app.post("/users", UserController.store);
 
 app.listen(3333, () => {
   console.log("Server running on port 3333");
