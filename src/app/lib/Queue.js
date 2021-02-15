a9dd6658e993cc4bfa0651faf59dcdb7c191d0fb
unknown <weeslleey354@gmail.com>
2021-02-15 20:29:25 -0300

    job options

--- src/app/lib/Queue.js
+++ src/app/lib/Queue.js
@@ -1,11 +1,13 @@
 import Queue from "bull";
 import redisConfig from "../../config/redis";
+
 import * as jobs from "../jobs";
 
 const queues = Object.values(jobs).map((job) => ({
   bull: new Queue(job.key, redisConfig),
   name: job.key,
   handle: job.handle,
+  options: job.options,
 }));
 
 export default {
@@ -13,14 +15,14 @@ export default {
   add(name, data) {
     const queue = this.queues.find((queue) => queue.name === name);
 
-    return queue.bull.add(data);
+    return queue.bull.add(data, queue.options);
   },
   process() {
     return this.queues.forEach((queue) => {
       queue.bull.process(queue.handle);
 
       queue.bull.on("failed", (job, err) => {
-        console.log("Job Failed", queue.key, job.data);
+        console.log("Job failed", queue.key, job.data);
         console.log(err);
       });
     });
--- src/config/Mail.js
+++ src/config/Mail.js
@@ -3,6 +3,6 @@ export default {
   port: process.env.MAIL_PORT,
   auth: {
     user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASSWORD,
+    pass: process.env.MAIL_PASS,
   },
 };
